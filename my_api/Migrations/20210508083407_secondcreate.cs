da1c034c4595d3f444179277f01c66dbcdf61de2
HarshitPradhan <59720787+HarshitPradhan@users.noreply.github.com>
2021-05-09 13:00:44 +0530

    Add project files.

---
+++ my_api/Migrations/20210508083407_secondcreate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace my_api.Migrations
+{
+    public partial class secondcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
